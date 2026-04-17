da5edbd3fed9ebc0ed7652b7b0deec242055e02a
nami-om <mariomheidemannvaz@gmail.com>
2026-04-16 22:13:23 -0300

    Js

---
+++ 01-javascript/testes.js
@@ -0,0 +1,22 @@
+// alert("Olá mundo!")
+// alert("SEGUNDA MENSAGEM")
+
+// let nome 
+
+// nome = "nami"
+
+// alert("Nome: " + nome)
+
+// let idade = 17
+// alert("Idade: " + idade)
+
+// alert("Nome: " + nome + "\nIdade: " + idade)
+
+// nome = prompt("Digite seu nome:")
+// alert("Nome digitado: " + nome)
+
+let nota1
+nota1 = prompt("Qual sua primeira nota? ")
+nota1 = Number(nota1)
+nota1 =  nota1 + 1
+alert(nota1)
