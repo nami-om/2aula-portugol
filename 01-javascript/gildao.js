282cd4ae34b85c740a3d3e6f1953c0b57a89df9a
nami-om <mariomheidemannvaz@gmail.com>
2026-04-27 22:06:52 -0300

    add

---
+++ 01-javascript/gildao.js
@@ -0,0 +1,7 @@
+let numePessoas, quantidadesTotais
+numePessoas = Number(prompt("Numero de pessoas: "))
+carne = 0.5 * numePessoas
+cerveja = 1 * numePessoas
+agua = 0.5 * numePessoas
+refri = 0.2 * numePessoas
+alert("Se o número de pessoas for: " + numePessoas + "\nEntão a quantidade será : " + "\nCarne: "+  carne + "\nCerveja: " + cerveja + "\nÁgua: " + agua + "\nRefri: " + refri)
