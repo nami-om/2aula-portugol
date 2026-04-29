a022b977d3618395f14f64aa46a8a6c9ed6a51c3
nami-om <mariomheidemannvaz@gmail.com>
2026-04-28 21:59:54 -0300

    css2

---
+++ 01-javascript/manojuca.js
@@ -0,0 +1,12 @@
+let moradia, luz, agua, gasolina, internet, streamings, telefone, outros, salario
+let saldo
+moradia = Number(prompt(1400))
+luz = Number(prompt(72))
+agua = Number(prompt(67))
+gasolina = Number(prompt(100))
+internet = Number(prompt(130))
+streamings = Number(prompt(98))
+telefone = Number(prompt(64))
+outros = Number(prompt(250))
+salario = Number(prompt("Salário: R$"))
+
--- 03-css/script.js
+++ 03-css/script.js
@@ -1,3 +1,13 @@
 function calcularPrecoBrique(){
-    alert("função executada")
+    //infos
+     let precoCompra, precoVenda
+    //leituras
+    precoCompra = Number(prompt("Preço de compra: "))
+    //processamento
+    precoVenda = precoCompra * 3
+    //saidas
+    console.log("Preço para venda: R$" + precoVenda.toFixed(2));
+    alert("Preço para venda: R$" + precoVenda.toFixed(2))
+
+    document.getElementById("resultado").innerHTML = "Preço para venda: R$" + precoVenda.toFixed(2)
 }
