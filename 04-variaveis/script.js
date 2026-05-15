8ad0b70cfafed720b5b9ea0e28907955be3a5c28
nami-om <mariomheidemannvaz@gmail.com>
2026-05-14 21:54:27 -0300

    variaveis

---
+++ 04-variaveis/script.js
@@ -0,0 +1,13 @@
+let global = 'Eu sou uma variavel global'
+function funcao1(){
+    let local = 'Sou uma variável local'
+    console.log(local);
+    console.log(global);
+}
+function funcao2(){
+    console.log(global);
+    //console.log(local);
+}
+
+funcao1()
+funcao2()
