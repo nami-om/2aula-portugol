970092b85f064cfa2b0fcc89b597d296429a5eae
MARIOM HEIDEMANN VAZ <mariom_vaz@estudante.sesisenai.org.br>
2026-06-15 21:37:05 -0300

    add

---
+++ 06-rock/script.js
@@ -0,0 +1,73 @@
+
+
+function iniciar(){
+const escolha = 0
+
+escolha = Number(prompt("\nRoque já tem a Jota Qwert confirmada e vai atrás da possível segunda atração do Rock in Reels." +
+
+                        "\nVisitando suas amigas da Tias Fofinhas (uma banda cover de Tear for Fears), fez o convite e ouviu suas exigências: 50 toalhas de crochê no camarim." +
+
+                        "\nRoque então se viu em um dilema: nem camarim o evento tem, elas vão tocar pela internet. Quanto mais 50 toalhas de crochê." +
+
+                        "\n\nRoque deve falar a verdade ou mentir que tem camarim e as toalhas." +
+
+                         "\n\n- Verdade: 02" +
+                         "\n- Mentir: 03"))
+if (escolha==2){
+escolha = Number(prompt("Roque contou para  as Tias Fofinhas que o evento ainda está engatinhando e não temos camarins ou mesmo verba para comprar toalhas de crochê, infelizmente." +
+
+                        "Elas ficaram tristes pela dura realidade dos músicos, mas apreciaram a sinceridade. Prometeram levar suas próprias toalhas e ainda passaram o contato do grupo de rap dos netos de uma amiga delas. Elas já viram Yasmina e os Manos se apresentando no aniversário da avó deles e acharam muito talentosos e carismáticos, além de muito conectados. Pode ser uma boa adição para este evento." +
+
+                        "Roque foi falar com eles, Yasmina pediu que o grupo fosse marcado em todas as fotos postadas de divulgação do evento, assim eles poderiam atrair seu público para o evento." +
+
+                        "Roque deve aceitar ou rejeitar essa exigência?" +
+
+                         "- Aceitar: 04" +
+                         "- Rejeitar: 05"))
+
+}if(escolha == 3){
+escolha = Number(prompt("Roque mente para as Tias Fofinhas que tem as toalhas. Elas perguntam de que cor são, ele diz que são brancas (afinal todas as toalhas de crochê são brancas, não é?). Elas complementam que suas toalhas tem que ser pretas. Eles gagueja, diz que se enganou e que elas são pretas." +
+
+                        "Elas desconfiam dele, fazem pressão para ver as toalhas e ele acaba reconhecendo que mentiu. Elas ficam muito decepcionadas e desistem de participar. Ao saber disso, a banda Jota Qwert também pula fora do barco." +
+
+                        "Ao ver Roque abatido com o iminente fracasso, seu pai, Lucio Fernando, oferece a Roque o estúdio e o salão da associação dos funcionários da sua fábrica, a Meia Meia Meia, para realizar o evento. Assinando um contrato ele também teria disponível o dinheiro necessário para trazer as bandas que quisesse." +
+
+                        "Roque nunca deixou o dinheiro e a influência de seu pai interferir em sua carreira artística e se sentiu incomodado com a proposta. Ao mesmo tempo, ficou balançado porque esse contrato seria a chance de realizar o maior sonho da sua vida!" +
+
+                        "oque deve assinar o contrato?" +
+
+                         "- Sim: 07" +
+                         "- Não: 06"))
+
+}if(escolha == 4){
+escolha = Number(prompt("Roque mente para as Tias Fofinhas que tem as toalhas. Elas perguntam de que cor são, ele diz que são brancas (afinal todas as toalhas de crochê são brancas, não é?). Elas complementam que suas toalhas tem que ser pretas. Eles gagueja, diz que se enganou e que elas são pretas." +
+
+                        "Elas desconfiam dele, fazem pressão para ver as toalhas e ele acaba reconhecendo que mentiu. Elas ficam muito decepcionadas e desistem de participar. Ao saber disso, a banda Jota Qwert também pula fora do barco." +
+
+                        "Ao ver Roque abatido com o iminente fracasso, seu pai, Lucio Fernando, oferece a Roque o estúdio e o salão da associação dos funcionários da sua fábrica, a Meia Meia Meia, para realizar o evento. Assinando um contrato ele também teria disponível o dinheiro necessário para trazer as bandas que quisesse." +
+
+                        "Roque nunca deixou o dinheiro e a influência de seu pai interferir em sua carreira artística e se sentiu incomodado com a proposta. Ao mesmo tempo, ficou balançado porque esse contrato seria a chance de realizar o maior sonho da sua vida!" +
+
+                        "oque deve assinar o contrato?" +
+
+                         "- Sim: 07" +
+                         "- Não: 06"))
+
+
+}if(escolha == 5){
+
+
+}if(escolha == 6){
+
+
+}if(escolha == 7){
+
+
+}if(escolha == 8){
+
+
+}if(escolha == 9){
+
+
+}
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+let contador = 0
+
+if(contador < 10){
+    alert("Contador é menor que 10")
+    alert("Fim")
+}//voltar e testar novamente 
