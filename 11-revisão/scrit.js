ebaa5e814c82438674ec8b6b2c6f6165ce5f4bdc
nami-om <mariomheidemannvaz@gmail.com>
2026-07-02 22:00:07 -0300

    add

---
+++ 11-revisão/scrit.js
@@ -0,0 +1,21 @@
+function contador(){
+    let i = 0, resposta = 0
+    while (i < 10){
+        i++
+        resposta = resposta + i
+        console.log(resposta)
+    }
+
+}
+
+function tabuada(){
+    let i = 0, multi = 5, resposta = 0
+    while (i<10) {
+        i++
+        resposta = i * multi
+        console.log(resposta)
+        
+    }
+
+}
+
